1edb744d2d26059896678ec35fca237815cafae6
Nana Kweku Adumatta <nkba@live.com>
2020-07-13 16:57:36 +0000

    Temo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import {PlayerComponent} from "./player/player.component";
-
-const routes: Routes = [
-    {path:"",redirectTo:"get-started",pathMatch:'full'},
-    { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) },
-    { path: "player", loadChildren: () => import("~/app/player/player.module").then((m) => m.PlayerModule) },
-    {path:"register",loadChildren:()=>import("~/app/register/register.module").then((m)=>m.RegisterModule)},
-    {path:"login",loadChildren:()=>import("~/app/login/login.module").then(m=>m.LoginModule)},
-    {path:"get-started",loadChildren:()=>import("~/app/get-started/get-started.module").then(m=>m.GetStartedModule)}
-];
-
-@NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
-})
-export class AppRoutingModule { }
-=======
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
@@ -43,4 +20,3 @@ const routes: Routes = [
     exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
->>>>>>> a4400b20f8b7ab94dc60df423640f2324d726c0c
